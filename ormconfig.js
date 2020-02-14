d67aae1425be99b239ecfadd4897edb94b4f327b
unknown <ptjh0309@hanmail.net>
2020-02-14 21:52:14 +0900

    Upload the ormconfig.js

---
+++ ormconfig.js
@@ -0,0 +1,16 @@
+﻿const { SnakeNamingStrategy } = require('typeorm-naming-strategies');
+
+module.exports = {
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "1234",
+    database: "fridayhellparty",
+    entities: [
+        './schemas/*.js'
+    ],
+    namingStrategy: new SnakeNamingStrategy(),
+    synchronize: true,
+    logging: true
+};
